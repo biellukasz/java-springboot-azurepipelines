3506b34b9ad6235ce2d662a37dc6211c50948195
lbiel <lbiel@oeconnection.com>
2020-04-30 19:57:27 +0200

    Initial commit

---
+++ src/test/java/com/example/javaspringbootazurepipelines/JavaSpringbootAzurepipelinesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootazurepipelines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootAzurepipelinesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
